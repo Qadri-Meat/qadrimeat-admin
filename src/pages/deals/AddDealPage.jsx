f0f2a8e876b283504819bc5f2b05612efaa4f689
Bilal Ahmed <bilal.ahmed@devprotocols.com>
2023-04-06 12:36:21 +0500

    conditionaly rendr add deal and update deal page

--- src/pages/deals/AddDealPage.jsx
+++ src/pages/deals/AddDealPage.jsx
@@ -1,15 +1,31 @@
 import AdminLayout from "@core/components/admin/AdminLayout/AdminLayout";
 import { Grid, Typography } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { getDeal } from "store/deal";
 
 const AddDealPage = () => {
+  const params = useParams();
+  const userId = params.id;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { details, success } = useSelector((state) => state.deal);
+  useEffect(() => {
+    if (userId !== "add-deal") dispatch(getDeal(userId));
+  }, [dispatch, userId]);
+
+  useEffect(() => {
+    if (success) {
+      navigate("/deals");
+    }
+  }, [dispatch, success, navigate]);
   return (
     <AdminLayout>
       <Grid container sx={{ my: 3 }} alignItems="center">
         <Grid item>
           <Typography variant="h5" component="h1">
-            Add New Deal
-            {/* {details ? "Update User" : "Create User"} */}
+            {details ? "Update Deal" : "Create Deal"}
           </Typography>
         </Grid>
       </Grid>
