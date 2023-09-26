e2501168b25c734475b71303530c23474756d5d4
yousaf Ramzan <yousaf.ramzan@devprotocols.com>
2023-09-26 12:25:01 +0500

    test: change schema for add stock form

--- src/pages/dailyreport/AddStockForm.js
+++ src/pages/dailyreport/AddStockForm.js
@@ -9,35 +9,15 @@ import FormInput from "@core/components/forms/FormInput";
 import { Button, Grid, MenuItem } from "@mui/material";
 import SaveIcon from "@mui/icons-material/Save";
 import SelectInput from "@core/components/forms/SelectInput";
+import withAuth from "hooks/withAuth";
 
 const schema = yup.object().shape({
   name: yup.string().required().max(20),
-  sku: yup.string().required(),
-  price: yup
-    .number()
-    .required()
-    .positive()
-    .typeError("Price is required field"),
-  stock: yup
-    .number()
-    .required()
-    .positive()
-    .typeError("Stock is required field"),
   weight: yup
     .number()
     .required()
     .positive()
     .typeError("Weight is required field"),
-  discount: yup
-    .number()
-    .moreThan(-1, "Discount must be a number greater than or equal to zero")
-    .required("Discount is a required field")
-    .typeError("Discount must be a number"),
-  saleCount: yup
-    .number()
-    .moreThan(-1, "Discount must be a number greater than or equal to zero")
-    .required("Discount is a required field")
-    .typeError("Discount must be a number"),
   category: yup
     .mixed()
     .test("isCategoryValid", "Category is a required field", function (value) {
@@ -89,28 +69,6 @@ const AddStockForm = () => {
         </Grid>
         <Grid item xs={12} md={4}>
           <FormInput
-            {...register("sku")}
-            id="sku"
-            type="text"
-            label="SKU"
-            name="sku"
-            error={!!errors.sku}
-            helperText={errors?.sku?.message}
-          />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <FormInput
-            {...register("price")}
-            id="price"
-            type="number"
-            label="Price"
-            name="price"
-            error={!!errors.price}
-            helperText={errors?.price?.message}
-          />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <FormInput
             {...register("weight")}
             id="weight"
             type="number"
@@ -118,61 +76,6 @@ const AddStockForm = () => {
             name="weight"
             error={!!errors.weight}
             helperText={errors?.weight?.message}
-          />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <FormInput
-            {...register("stock")}
-            id="stock"
-            type="number"
-            label="Stock"
-            name="stock"
-            error={!!errors.stock}
-            helperText={errors?.stock?.message}
-          />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <FormInput
-            {...register("discount")}
-            id="discount"
-            type="number"
-            label="Discount"
-            name="discount"
-            error={!!errors.discount}
-            helperText={errors?.discount?.message}
-          />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <FormInput
-            {...register("saleCount")}
-            id="saleCount"
-            type="number"
-            label="Sale Count"
-            name="saleCount"
-            error={!!errors.saleCount}
-            helperText={errors?.saleCount?.message}
-          />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <FormInput
-            {...register("fullDescription")}
-            id="fullDescription"
-            type="fullDescription"
-            label="Full Description"
-            name="fullDescription"
-            error={!!errors.fullDescription}
-            helperText={errors?.fullDescription?.message}
-          />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <FormInput
-            {...register("shortDescription")}
-            id="shortDescription"
-            type="shortDescription"
-            label="Short Description"
-            name="shortDescription"
-            error={!!errors.shortDescription}
-            helperText={errors?.shortDescription?.message}
           />
         </Grid>
 
@@ -207,4 +110,4 @@ const AddStockForm = () => {
   );
 };
 
-export default AddStockForm;
+export default withAuth(AddStockForm);
