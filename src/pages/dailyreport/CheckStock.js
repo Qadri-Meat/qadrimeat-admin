75674fb28a6f90931e21fa300f871c1f29e2a286
yousaf Ramzan <yousaf.ramzan@devprotocols.com>
2023-09-26 16:36:56 +0500

    test: add checkout page

---
+++ src/pages/dailyreport/CheckStock.js
@@ -0,0 +1,67 @@
+import React, { useEffect } from "react";
+import AdminLayout from "@core/components/admin/AdminLayout/AdminLayout";
+import AdminBreadcrumbs from "@core/components/admin/AdminBreadcrumbs/AdminBreadcrumbs";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  Paper,
+  Typography,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { getDashboard } from "store/user";
+import withAuth from "hooks/withAuth";
+import BookingTable from "pages/dashboard/components/BookingTable";
+
+const CheckStock = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getDashboard());
+  }, [dispatch]);
+
+  return (
+    <AdminLayout>
+      <h2>Dashboard</h2>
+      <AdminBreadcrumbs />
+      <Grid container spacing={2}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={4}>
+            <Card>
+              <CardContent>
+                <Typography variant="h5" component="div">
+                  Today's Report
+                </Typography>
+                <Typography
+                  sx={{ mb: 1.5 }}
+                  color="text.secondary"
+                ></Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" onClick={() => navigate("/bookings")}>
+                  View All
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} md={4}></Grid>
+        </Grid>
+      </Grid>
+      <Grid style={{ marginTop: 20 }} container spacing={2}>
+        <Grid item xs={12} md={7}>
+          <Paper style={{ padding: 10, marginTop: 15 }} variant="outlined">
+            <Typography variant="h5">Total Report</Typography>
+            <BookingTable />
+          </Paper>
+        </Grid>
+      </Grid>
+    </AdminLayout>
+  );
+};
+
+export default withAuth(CheckStock);
--- src/pages/dailyreport/DailyReportPageConfig.js
+++ src/pages/dailyreport/DailyReportPageConfig.js
@@ -1,10 +1,16 @@
 import AddStockForm from "./AddStockForm";
+import CheckStock from "./CheckStock";
 export const DailyReportPageConfig = {
   routes: [
     {
       path: "/addstock",
       exact: true,
       element: <AddStockForm />,
+    },
+    {
+      path: "/checkstock",
+      exact: true,
+      element: <CheckStock />,
     },
   ],
 };
