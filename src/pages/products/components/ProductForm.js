edcb2ff2b7bb3a5dd549e41ccb93cdd09937e17a
yousaf Ramzan <yousaf.ramzan@devprotocols.com>
2023-10-04 16:17:03 +0500

    test: add alert on more then five images on the add product screen(Not working)

--- src/pages/products/components/ProductForm.js
+++ src/pages/products/components/ProductForm.js
@@ -10,9 +10,10 @@ import { Button, Grid, MenuItem } from '@mui/material';
 import SaveIcon from '@mui/icons-material/Save';
 import SelectInput from '@core/components/forms/SelectInput';
 import { DropzoneArea } from 'material-ui-dropzone';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { createProducts, updateProducts } from 'store/product';
 import { getImageUrl, isValidImages } from 'helper/helpers';
+import Swal from 'sweetalert2'; // Import SweetAlert
 
 const schema = yup.object().shape({
   name: yup.string().required().max(20),
@@ -67,6 +68,7 @@ const schema = yup.object().shape({
 const ProductForm = ({ defaultValues }) => {
   const dispatch = useDispatch();
   const [files, setFiles] = useState([]);
+  const [showAlert, setShowAlert] = useState(false);
   const { message, loading } = useSelector((state) => state.product);
 
   const initialFiles = (defaultValues?.image || []).map(
@@ -96,8 +98,17 @@ const ProductForm = ({ defaultValues }) => {
 
   const handleChange = (newFiles) => {
     if (isValidImages(newFiles)) {
-      setFiles(newFiles);
+      if (newFiles.length <= 5) {
+        setFiles(newFiles);
+      } else {
+        setShowAlert(true);
+        setFiles([]);
+      }
     }
+  };
+
+  const handleCloseAlert = () => {
+    setShowAlert(false);
   };
 
   const onSubmit = (data) => {
@@ -111,149 +122,159 @@ const ProductForm = ({ defaultValues }) => {
   };
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      {message && <Message severity="error">{message}</Message>}
+    <>
+      {showAlert &&
+        Swal.fire({
+          title: 'Oops...',
+          text: 'You can only upload up to 5 images!',
+          icon: 'error',
+        }).then(() => {
+          handleCloseAlert();
+        })}
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        {message && <Message severity="error">{message}</Message>}
 
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={4}>
-          <FormInput
-            {...register('name')}
-            id="name"
-            type="text"
-            label="Name"
-            name="name"
-            error={!!errors.name}
-            helperText={errors?.name?.message}
-          />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <FormInput
-            {...register('sku')}
-            id="sku"
-            type="text"
-            label="SKU"
-            name="sku"
-            error={!!errors.sku}
-            helperText={errors?.sku?.message}
-          />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <FormInput
-            {...register('price')}
-            id="price"
-            type="number"
-            label="Price"
-            name="price"
-            error={!!errors.price}
-            helperText={errors?.price?.message}
-          />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <FormInput
-            {...register('weight')}
-            id="weight"
-            type="number"
-            label="Weight (KG)"
-            name="weight"
-            error={!!errors.weight}
-            helperText={errors?.weight?.message}
-          />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <FormInput
-            {...register('discount')}
-            id="discount"
-            type="number"
-            label="Discount"
-            name="discount"
-            error={!!errors.discount}
-            helperText={errors?.discount?.message}
-          />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <FormInput
-            {...register('saleCount')}
-            id="saleCount"
-            type="number"
-            label="Sale Count"
-            name="saleCount"
-            error={!!errors.saleCount}
-            helperText={errors?.saleCount?.message}
-          />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <FormInput
-            {...register('fullDescription')}
-            id="fullDescription"
-            type="fullDescription"
-            label="Full Description"
-            name="fullDescription"
-            error={!!errors.fullDescription}
-            helperText={errors?.fullDescription?.message}
-          />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <FormInput
-            {...register('shortDescription')}
-            id="shortDescription"
-            type="shortDescription"
-            label="Short Description"
-            name="shortDescription"
-            error={!!errors.shortDescription}
-            helperText={errors?.shortDescription?.message}
-          />
-        </Grid>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={4}>
+            <FormInput
+              {...register('name')}
+              id="name"
+              type="text"
+              label="Name"
+              name="name"
+              error={!!errors.name}
+              helperText={errors?.name?.message}
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <FormInput
+              {...register('sku')}
+              id="sku"
+              type="text"
+              label="SKU"
+              name="sku"
+              error={!!errors.sku}
+              helperText={errors?.sku?.message}
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <FormInput
+              {...register('price')}
+              id="price"
+              type="number"
+              label="Price"
+              name="price"
+              error={!!errors.price}
+              helperText={errors?.price?.message}
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <FormInput
+              {...register('weight')}
+              id="weight"
+              type="number"
+              label="Weight (KG)"
+              name="weight"
+              error={!!errors.weight}
+              helperText={errors?.weight?.message}
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <FormInput
+              {...register('discount')}
+              id="discount"
+              type="number"
+              label="Discount"
+              name="discount"
+              error={!!errors.discount}
+              helperText={errors?.discount?.message}
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <FormInput
+              {...register('saleCount')}
+              id="saleCount"
+              type="number"
+              label="Sale Count"
+              name="saleCount"
+              error={!!errors.saleCount}
+              helperText={errors?.saleCount?.message}
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <FormInput
+              {...register('fullDescription')}
+              id="fullDescription"
+              type="fullDescription"
+              label="Full Description"
+              name="fullDescription"
+              error={!!errors.fullDescription}
+              helperText={errors?.fullDescription?.message}
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <FormInput
+              {...register('shortDescription')}
+              id="shortDescription"
+              type="shortDescription"
+              label="Short Description"
+              name="shortDescription"
+              error={!!errors.shortDescription}
+              helperText={errors?.shortDescription?.message}
+            />
+          </Grid>
 
-        <Grid item xs={12} md={4}>
-          <SelectInput
-            {...register('category')}
-            id="category"
-            name="category"
-            label="Category"
-            control={control}
-            error={!!errors.category}
-            helperText={errors?.category?.message}
-          >
-            <MenuItem value="chicken">Chicken</MenuItem>
-            <MenuItem value="beef">Beef</MenuItem>
-            <MenuItem value="mutton">Mutton</MenuItem>
-          </SelectInput>
+          <Grid item xs={12} md={4}>
+            <SelectInput
+              {...register('category')}
+              id="category"
+              name="category"
+              label="Category"
+              control={control}
+              error={!!errors.category}
+              helperText={errors?.category?.message}
+            >
+              <MenuItem value="chicken">Chicken</MenuItem>
+              <MenuItem value="beef">Beef</MenuItem>
+              <MenuItem value="mutton">Mutton</MenuItem>
+            </SelectInput>
+          </Grid>
+          <Grid item xs={12}>
+            <DropzoneArea
+              maxFileSize={5242880}
+              onChange={handleChange}
+              showAlerts={false}
+              filesLimit={5}
+              dropzoneText=""
+              initialFiles={initialFiles}
+              acceptedFiles={[
+                'image/jpeg',
+                'image/jpg',
+                'image/png',
+                'image/gif',
+              ]}
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ textAlign: 'center' }}>
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+              size="large"
+              endIcon={<SaveIcon />}
+            >
+              {loading ? (
+                <Loader />
+              ) : defaultValues ? (
+                'Update Product'
+              ) : (
+                'Save Product'
+              )}
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <DropzoneArea
-            maxFileSize={5242880}
-            onChange={handleChange}
-            showAlerts={false}
-            filesLimit={5}
-            dropzoneText=""
-            initialFiles={initialFiles}
-            acceptedFiles={[
-              'image/jpeg',
-              'image/jpg',
-              'image/png',
-              'image/gif',
-            ]}
-          />
-        </Grid>
-        <Grid item xs={12} sx={{ textAlign: 'center' }}>
-          <Button
-            variant="contained"
-            color="primary"
-            type="submit"
-            size="large"
-            endIcon={<SaveIcon />}
-          >
-            {loading ? (
-              <Loader />
-            ) : defaultValues ? (
-              'Update Product'
-            ) : (
-              'Save Product'
-            )}
-          </Button>
-        </Grid>
-      </Grid>
-    </Form>
+      </Form>
+    </>
   );
 };
 
