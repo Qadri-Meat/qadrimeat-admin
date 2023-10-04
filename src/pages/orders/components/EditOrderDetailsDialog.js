38725df21082401e6653b53c4418a5638d086bc7
yousaf Ramzan <yousaf.ramzan@devprotocols.com>
2023-10-04 18:26:24 +0500

    test: On update shipping details form, Add cancel or small cross button

--- src/pages/orders/components/EditOrderDetailsDialog.js
+++ src/pages/orders/components/EditOrderDetailsDialog.js
@@ -4,32 +4,39 @@ import {
   DialogActions,
   DialogContent,
   DialogTitle,
-} from "@material-ui/core";
-import React from "react";
-import SaveIcon from "@material-ui/icons/Save";
-import * as yup from "yup";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useDispatch, useSelector } from "react-redux";
-import CustomTextField from "@core/components/admin/CustomTextField";
-import Loader from "@core/components/ui/Loader";
-import { Message } from "@mui/icons-material";
-import Form from "@core/components/forms/Form";
-import { updateOrder } from "store/order";
+} from '@material-ui/core';
+import React from 'react';
+import SaveIcon from '@material-ui/icons/Save';
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useDispatch, useSelector } from 'react-redux';
+import CustomTextField from '@core/components/admin/CustomTextField';
+import Loader from '@core/components/ui/Loader';
+import { Message } from '@mui/icons-material';
+import Form from '@core/components/forms/Form';
+import { updateOrder } from 'store/order';
 
 const schema = yup.object().shape({
   firstName: yup.string(),
   lastName: yup.string(),
   phone: yup
     .string()
-    .matches(/^\+?\d{1,4}[-.\s]?\d{1,14}$/i, "Invalid phone number"),
+    .matches(/^\+?\d{1,4}[-.\s]?\d{1,14}$/i, 'Invalid phone number'),
   address: yup.string(),
   city: yup.string(),
-  postalCode: yup.string().matches(/^[a-zA-Z0-9\s-]*$/, "Invalid postal code"),
+  postalCode: yup
+    .string()
+    .matches(/^[a-zA-Z0-9\s-]*$/, 'Invalid postal code'),
   country: yup.string(),
 });
 
-const EditOrderDetailsDialog = ({ show, setShow, preloadedValues, id }) => {
+const EditOrderDetailsDialog = ({
+  show,
+  setShow,
+  preloadedValues,
+  id,
+}) => {
   const { error, loading } = useSelector((state) => state.order);
   const {
     control,
@@ -37,7 +44,7 @@ const EditOrderDetailsDialog = ({ show, setShow, preloadedValues, id }) => {
     formState: { errors },
   } = useForm({
     defaultValues: preloadedValues,
-    mode: "onBlur",
+    mode: 'onBlur',
     resolver: yupResolver(schema),
   });
   const dispatch = useDispatch();
@@ -131,12 +138,21 @@ const EditOrderDetailsDialog = ({ show, setShow, preloadedValues, id }) => {
             <Button
               variant="contained"
               color="primary"
+              onClick={handleClose} // Close the dialog
+              size="large"
+              style={{ marginLeft: '8px' }}
+            >
+              Close
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
               type="submit"
               size="large"
               endIcon={<SaveIcon />}
-              style={{ marginLeft: "8px" }}
+              style={{ marginLeft: '8px' }}
             >
-              {loading ? <Loader /> : "Update"}
+              {loading ? <Loader /> : 'Update'}
             </Button>
           </DialogActions>
         </Form>
