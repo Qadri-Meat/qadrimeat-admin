5f027204a812779faefd9e853ebdf562167282e1
yousaf Ramzan <yousaf.ramzan@devprotocols.com>
2023-10-12 17:08:03 +0500

    test: add clear filter button on the order page screen

--- src/pages/orders/components/OrderPageHeading.js
+++ src/pages/orders/components/OrderPageHeading.js
@@ -14,21 +14,24 @@ const OrderPageHeading = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const { paid } = pick(location.search);
+  const handleResetFilter = (event, value) => {
+    navigate('/orders');
+  };
   const handlePaidToggle = (event, value) => {
     navigate(`/orders?paid=${value}`);
-
-    // useEffect(() => {
-    //   if (success) {
-    //     dispatch(resetBooking());
-    //   } else {
-    //     dispatch(
-    //       getBookings(
-    //         `${paid !== undefined ? `isPaid=${paid}&` : ''}${query}`
-    //       )
-    //     );
-    //   }
-    // }, [dispatch, paid, query, success]);
   };
+
+  // useEffect(() => {
+  //   if (success) {
+  //     dispatch(resetBooking());
+  //   } else {
+  //     dispatch(
+  //       getBookings(
+  //         `${paid !== undefined ? `isPaid=${paid}&` : ''}${query}`
+  //       )
+  //     );
+  //   }
+  // }, [dispatch, paid, query, success]);
   return (
     <Grid container sx={{ my: 3 }} gap={1} alignItems="center">
       <Grid item>
@@ -52,6 +55,16 @@ const OrderPageHeading = () => {
             size="small"
           >
             Add Order
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button
+            onClick={handleResetFilter}
+            variant="outlined"
+            color="primary"
+            size="small"
+          >
+            Clear Filter
           </Button>
         </Grid>
         <Grid item>
