cd010f13a98000bb7712250325e11f8a341b33eb
Zubair Sheikh <zubair@xertk.com>
2024-04-23 14:20:28 +0500

    fix: update image path

--- src/helper/helpers.js
+++ src/helper/helpers.js
@@ -1,6 +1,9 @@
 export const isNullOrEmpty = (value) => {
   return (
-    value === null || value === undefined || value === "" || value === "null"
+    value === null ||
+    value === undefined ||
+    value === '' ||
+    value === 'null'
   );
 };
 
@@ -9,33 +12,37 @@ export const isArray = (value) => {
 };
 
 export const isObject = (value) => {
-  return value !== null && typeof value === "object" && !Array.isArray(value);
+  return (
+    value !== null &&
+    typeof value === 'object' &&
+    !Array.isArray(value)
+  );
 };
 
 export const isFunction = (value) => {
-  return typeof value === "function";
+  return typeof value === 'function';
 };
 
 export const isString = (value) => {
-  return typeof value === "string";
+  return typeof value === 'string';
 };
 
 export const isNumber = (value) => {
-  return typeof value === "number" && isFinite(value);
+  return typeof value === 'number' && isFinite(value);
 };
 
 export const getImageUrl = (image) => {
   if (!isNullOrEmpty(image)) {
-    return `${process.env.REACT_APP_API_URL}/v1/${image}`;
+    return image;
   }
-  return "/default.png";
+  return '/default.png';
 };
 
 export const isValidImages = (files) => {
-  const allowedExtensions = ["jpg", "jpeg", "png", "gif", "bmp"]; // Add more extensions if needed
+  const allowedExtensions = ['jpg', 'jpeg', 'png', 'gif', 'bmp']; // Add more extensions if needed
   files.forEach((file) => {
     const fileName = file.name.toLowerCase();
-    const fileExtension = fileName.split(".").pop();
+    const fileExtension = fileName.split('.').pop();
     if (!allowedExtensions.includes(fileExtension)) {
       return false;
     }
